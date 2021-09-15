72560df32231b7a64535eb330b86ce729a951e5e
Ebru Şakar <61549037+ebrusakar@users.noreply.github.com>
2021-09-15 23:21:23 +0300

    Add files via upload

---
+++ basic7.sql
@@ -0,0 +1,5 @@
+SELECT rating FROM film GROUP BY RATING;
+SELECT replacement_cost, COUNT(*) FROM film GROUP BY replacement_cost HAVING COUNT(*)>50;
+SELECT store_id, COUNT(*) FROM customer GROUP BY store_id;
+SELECT country_id, COUNT(*) FROM city GROUP BY country_id ORDER BY COUNT(*) DESC LIMIT 1;
+
