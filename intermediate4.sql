35b29ab8a4ce2f9fe22e827cacb53373a76bd358
Ebru Şakar <61549037+ebrusakar@users.noreply.github.com>
2021-09-16 17:48:17 +0300

    Update intermediate4.sql

--- intermediate4.sql
+++ intermediate4.sql
@@ -9,3 +9,15 @@ INTERSECT
 (SELECT first_name FROM actor)
 EXCEPT
 (SELECT first_name FROM customer);
+
+(SELECT first_name FROM actor)
+UNION ALL
+(SELECT first_name FROM customer);
+
+(SELECT first_name FROM actor)
+INTERSECT ALL
+(SELECT first_name FROM customer);
+
+(SELECT first_name FROM actor)
+EXCEPT ALL
+(SELECT first_name FROM customer);
