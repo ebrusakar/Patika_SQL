343d81dd5b9ea7395243f571f25b99684499c8d2
Ebru Şakar <61549037+ebrusakar@users.noreply.github.com>
2021-09-19 18:20:24 +0300

    Update basic6.sql

--- basic6.sql
+++ basic6.sql
@@ -1,3 +1,12 @@
+Ödev 6
+
+Aşağıdaki sorgu senaryolarını dvdrental örnek veri tabanı üzerinden gerçekleştiriniz.
+
+film tablosunda bulunan rental_rate sütunundaki değerlerin ortalaması nedir?
+film tablosunda bulunan filmlerden kaçtanesi 'C' karekteri ile başlar?
+film tablosunda bulunan filmlerden rental_rate değeri 0.99 a eşit olan en uzun (length) film kaç dakikadır?
+film tablosunda bulunan filmlerin uzunluğu 150 dakikadan büyük olanlarına ait kaç farklı replacement_cost değeri vardır?
+
 SELECT AVG(rental_rate) 
 FROM film;
 
