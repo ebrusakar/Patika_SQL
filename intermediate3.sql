e304549e336584ad566b3594d7ebaf6e62c6a199
Ebru Şakar <61549037+ebrusakar@users.noreply.github.com>
2021-09-19 18:25:22 +0300

    Update intermediate3.sql

--- intermediate3.sql
+++ intermediate3.sql
@@ -1,3 +1,11 @@
+Ödev 10
+
+Aşağıdaki sorgu senaryolarını dvdrental örnek veri tabanı üzerinden gerçekleştiriniz.
+
+1.city tablosu ile country tablosunda bulunan şehir (city) ve ülke (country) isimlerini birlikte görebileceğimiz LEFT JOIN sorgusunu yazınız.
+2.customer tablosu ile payment tablosunda bulunan payment_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz RIGHT JOIN sorgusunu yazınız.
+3.customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz FULL JOIN sorgusunu yazınız.
+
 SELECT a.city, b.country, a.country_id, b.country_id FROM city a
 LEFT JOIN country b ON a.country_id = b.country_id;
 
