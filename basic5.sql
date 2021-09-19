cdfe9e68883ffd9b238649b790a32e52d4e11c52
Ebru Şakar <61549037+ebrusakar@users.noreply.github.com>
2021-09-19 18:19:30 +0300

    Update basic5.sql

--- basic5.sql
+++ basic5.sql
@@ -1,3 +1,11 @@
+Ödev 5
+
+Aşağıdaki sorgu senaryolarını dvdrental örnek veri tabanı üzerinden gerçekleştiriniz.
+
+film tablosunda bulunan ve film ismi (title) 'n' karakteri ile biten en uzun (length) 5 filmi sıralayınız.
+film tablosunda bulunan ve film ismi (title) 'n' karakteri ile biten en kısa (length) ikinci 5 filmi sıralayınız.
+customer tablosunda bulunan last_name sütununa göre azalan yapılan sıralamada store_id 1 olmak koşuluyla ilk 4 veriyi sıralayınız.
+
 SELECT TITLE,LENGTH FROM film
 WHERE title LIKE '%n'
 ORDER BY length DESC
