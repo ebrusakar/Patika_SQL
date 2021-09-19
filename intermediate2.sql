c6274ac0e306a3a38640a200e49d58a13d495da5
Ebru Şakar <61549037+ebrusakar@users.noreply.github.com>
2021-09-19 18:24:15 +0300

    Update intermediate2.sql

--- intermediate2.sql
+++ intermediate2.sql
@@ -1,3 +1,11 @@
+Ödev 9
+
+Aşağıdaki sorgu senaryolarını dvdrental örnek veri tabanı üzerinden gerçekleştiriniz.
+
+1.city tablosu ile country tablosunda bulunan şehir (city) ve ülke (country) isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+2.customer tablosu ile payment tablosunda bulunan payment_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+3.customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+
 SELECT a.city, b.country, a.country_id, b.country_id FROM city a
 INNER JOIN country b ON a.country_id = b.country_id;
 
