3f7ad4239f141abe5fe888c8001ca625926e326a
Ebru Şakar <61549037+ebrusakar@users.noreply.github.com>
2021-09-19 18:21:43 +0300

    Update basic7.sql

--- basic7.sql
+++ basic7.sql
@@ -1,3 +1,13 @@
+Ödev 7
+
+Aşağıdaki sorgu senaryolarını dvdrental örnek veri tabanı üzerinden gerçekleştiriniz.
+
+1.film tablosunda bulunan filmleri rating değerlerine göre gruplayınız.
+2.film tablosunda bulunan filmleri replacement_cost sütununa göre grupladığımızda film sayısı 50 den fazla olan replacement_cost değerini ve karşılık gelen
+  film sayısını sıralayınız.
+3. customer tablosunda bulunan store_id değerlerine karşılık gelen müşteri sayılarını nelerdir?
+4. city tablosunda bulunan şehir verilerini country_id sütununa göre gruplandırdıktan sonra en fazla şehir sayısı barındıra country_id bilgisini ve şehir sayısını paylaşınız.
+
 SELECT rating FROM film GROUP BY RATING;
 SELECT replacement_cost, COUNT(*) FROM film GROUP BY replacement_cost HAVING COUNT(*)>50;
 SELECT store_id, COUNT(*) FROM customer GROUP BY store_id;
