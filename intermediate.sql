e203314a02ef1f6de6816a5d98175247a21d0c77
Ebru Şakar <61549037+ebrusakar@users.noreply.github.com>
2021-09-19 18:23:10 +0300

    Update intermediate.sql

--- intermediate.sql
+++ intermediate.sql
@@ -1,3 +1,10 @@
+Ödev 8
+
+1.test veritabanınızda employee isimli sütun bilgileri id(INTEGER), name VARCHAR(50), birthday DATE, email VARCHAR(100) olan bir tablo oluşturalım.
+2.Oluşturduğumuz employee tablosuna 'Mockaroo' servisini kullanarak 50 adet veri ekleyelim.
+3.Sütunların her birine göre diğer sütunları güncelleyecek 5 adet UPDATE işlemi yapalım.
+4.Sütunların her birine göre ilgili satırı silecek 5 adet DELETE işlemi yapalım.
+
 CREATE TABLE employee (
   id INTEGER PRIMARY KEY,
   name VARCHAR(50) NOT NULL,
