1c948ee0caff9f2b84b390510c1c52ae2c7ef52a
Ebru Şakar <61549037+ebrusakar@users.noreply.github.com>
2021-09-19 18:26:48 +0300

    Update intermediate4.sql

--- intermediate4.sql
+++ intermediate4.sql
@@ -1,3 +1,12 @@
+Ödev 11
+
+Aşağıdaki sorgu senaryolarını dvdrental örnek veri tabanı üzerinden gerçekleştiriniz.
+
+1.actor ve customer tablolarında bulunan first_name sütunları için tüm verileri sıralayalım.
+2.actor ve customer tablolarında bulunan first_name sütunları için kesişen verileri sıralayalım.
+3.actor ve customer tablolarında bulunan first_name sütunları için ilk tabloda bulunan ancak ikinci tabloda bulunmayan verileri sıralayalım.
+4.İlk 3 sorguyu tekrar eden veriler için de yapalım.
+
 (SELECT first_name FROM actor)
 UNION
 (SELECT first_name FROM customer);
