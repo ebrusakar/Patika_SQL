59271dbc061ff87b9393e0eb3f43403b2ee37ad8
Ebru Şakar <61549037+ebrusakar@users.noreply.github.com>
2021-09-19 18:28:09 +0300

    Update intermediate5.sql

--- intermediate5.sql
+++ intermediate5.sql
@@ -1,3 +1,12 @@
+Ödev 12
+
+Aşağıdaki sorgu senaryolarını dvdrental örnek veri tabanı üzerinden gerçekleştiriniz.
+
+1.film tablosunda film uzunluğu length sütununda gösterilmektedir. Uzunluğu ortalama film uzunluğundan fazla kaç tane film vardır?
+2.film tablosunda en yüksek rental_rate değerine sahip kaç tane film vardır?
+3.film tablosunda en düşük rental_rate ve en düşün replacement_cost değerlerine sahip filmleri sıralayınız.
+4.payment tablosunda en fazla sayıda alışveriş yapan müşterileri(customer) sıralayınız.
+
 SELECT COUNT (*) AS COUNT
 FROM FILM
 WHERE LENGTH >
