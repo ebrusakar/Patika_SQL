780314c923c3df044fa4f1823d3f333ecd207227
Ebru Şakar <61549037+ebrusakar@users.noreply.github.com>
2021-09-19 18:15:30 +0300

    Update basic2.sql

--- basic2.sql
+++ basic2.sql
@@ -1,3 +1,13 @@
+
+Ödev 2
+
+Aşağıdaki sorgu senaryolarını dvdrental örnek veri tabanı üzerinden gerçekleştiriniz.
+
+
+film tablosunda bulunan tüm sütunlardaki verileri replacement cost değeri 12.99 dan büyük eşit ve 16.99 küçük olma koşuluyla sıralayınız ( BETWEEN - AND yapısını kullanınız.)
+actor tablosunda bulunan first_name ve last_name sütunlardaki verileri first_name 'Penelope' veya 'Nick' veya 'Ed' değerleri olması koşuluyla sıralayınız. ( IN operatörünü kullanınız.)
+film tablosunda bulunan tüm sütunlardaki verileri rental_rate 0.99, 2.99, 4.99 VE replacement_cost 12.99, 15.99, 28.99 olma koşullarıyla sıralayınız. ( IN operatörünü kullanınız.)
+
 SELECT * FROM FILM WHERE replacement_cost BETWEEN  12.99 AND 16.99;
 SELECT first_name, last_name FROM ACTOR WHERE first_name IN  ('Penelope' , 'Nick' , 'Ed');
 SELECT * FROM FILM WHERE replacement_cost IN (12.99, 15.99, 28.99) AND rental_rate IN (0.99, 2.99, 4.99); 
